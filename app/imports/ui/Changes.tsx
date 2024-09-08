500eef4dc10164aac1d4a00621ce26875b07b12c
Matthias Roggo <matthias.roggo@posteo.ch>
2024-09-08 21:45:47 +0200

    changes

--- app/imports/ui/Changes.tsx
+++ app/imports/ui/Changes.tsx
@@ -1,12 +1,31 @@
 import TrackingDocumentTitle from "/imports/ui/TrackingDocumentTitle";
-import React from "react";
-import {FactorContext} from "/imports/ui/Viewer";
+import React, {useContext} from "react";
+import {Rezepte} from "/imports/api/models/rezept";
+import {DataLoadingContext} from "/imports/ui/ContentWrapper";
+import {DateTime} from "luxon";
+import {Link} from "react-router-dom";
 
-export function Changes() {
-  return (<>
-    <TrackingDocumentTitle title="Aktuelles" />
+
+export const Changes = () => {
+  let isLoading = useContext(DataLoadingContext);
+
+  const rezepte = Rezepte.find({createdAt: {$exists: true}}, { sort: { createdAt: -1 }, limit: 20 }).fetch().map(rezept => {
+    const date = rezept.createdAt ? DateTime.fromJSDate(rezept.createdAt).toRelative() : ''
+    return <li><Link to={'/' + rezept.slug}>{rezept.name}</Link>, {date}</li>;
+    }
+  );
+  const count = Rezepte.find().count();
+
+  return isLoading ? 'lade' : <>
+    <TrackingDocumentTitle title="Aktuelles"/>
     <div className="page">
-        <h1>Aktuelles</h1>
-      </div>
-  </>);
-}
+      <h1>Aktuelles!</h1>
+      Toll, es gibt hier {count} Rezepte zu entdecken.
+
+      <h2>Kürzliche Geändert</h2>
+      <ul>
+        {rezepte}
+      </ul>
+    </div>
+  </>;
+};
